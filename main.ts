3aabfe968c1b2fd810bc8e3918f3d00477ab3e48
Mdarfaan5178 <mdarfaan5178@gmail.com>
2024-05-16 11:16:14 +0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let text_list = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (["Nuni", "Gvvuygyyj", "Uuyuuy1w"]))
+})
+input.onButtonPressed(Button.B, function () {
+    text_list = 0
+})
